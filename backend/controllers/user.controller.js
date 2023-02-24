f10e3fc74c8eb537a16fcf8075bc24086b41141c
Escanor1986 <lionel.zovi@gmail.com>
2023-02-24 14:25:17 +0100

    final userController fixed issue

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+// Utilisation de bcrypt mais nous aurions pu utiliser argon2 également
 const bcrypt = require("bcrypt");
 const User = require("../models/user.models");
 const passwordSchema = require("../config/password.config");
@@ -44,42 +45,6 @@ exports.signup = async (req, res, next) => {
   }
 };
 
-// exports.signup = (req, res, next) => {
-//   const emailMask2Options = {
-//     maskWith: "*",
-//     unmaskedStartCharactersBeforeAt: 3,
-//     unmaskedEndCharactersAfterAt: 2,
-//     maskAtTheRate: false,
-//   };
-//   const email = req.body.email;
-//   const maskedEmail = MaskData.maskEmail2(email, emailMask2Options);
-//   const checkedPassword = passwordSchema.validate(req.body.password);
-//   const checkedEmail = emailValidator.validate(req.body.email);
-//   if (!checkedEmail && !checkedPassword) {
-//     console.log("Email et/ou Password invalide !");
-//   } else {
-//     const salt = bcrypt.genSaltSync(10);
-//     bcrypt
-//       .hash(req.body.password, salt)
-//       .then((hash) => {
-//         const user = new User({
-//           email: maskedEmail,
-//           password: hash,
-//         });
-//         user
-//           .save()
-//           .then(() =>
-//             res.status(201).json({
-//               message:
-//                 "Nouvel utilisateur créé et enregistré dans la base de données avec succès !",
-//             })
-//           )
-//           .catch((error) => res.status(400).json({ error }));
-//       })
-//       .catch((error) => res.status(500).json({ error }));
-//   }
-// };
-
 // Concernant le LOGIN ********************************************************************************
 // ****************************************************************************************************
 
@@ -120,39 +85,3 @@ exports.login = async (req, res, next) => {
     return res.status(500).json({ error: "Erreur interne du serveur" });
   }
 };
-
-// exports.login = (req, res, next) => {
-//   const emailMask2Options = {
-//     maskWith: "*",
-//     unmaskedStartCharactersBeforeAt: 3,
-//     unmaskedEndCharactersAfterAt: 2,
-//     maskAtTheRate: false,
-//   };
-//   const email = req.body.email;
-//   const maskedEmail = MaskData.maskEmail2(email, emailMask2Options);
-//   User.findOne({ email: maskedEmail })
-//     .then((user) => {
-//       if (!user) {
-//         console.log("Utilisateur non trouvé !");
-//         return res.status(401).json({ error });
-//       } else {
-//         bcrypt
-//           .compare(req.body.password, user.password)
-//           .then((valid) => {
-//             if (!valid) {
-//               console.log("Mot de passe incorrect !");
-//               return res.status(401).json({ error });
-//             } else {
-//               res.status(200).json({
-//                 userId: user._id,
-//                 token: jwt.sign({ userId: user._id }, process.env.SECRET, {
-//                   expiresIn: "24h",
-//                 }),
-//               });
-//             }
-//           })
-//           .catch((error) => res.status(500).json({ error }));
-//       }
-//     })
-//     .catch((error) => res.status(500).json({ error }));
-// };
